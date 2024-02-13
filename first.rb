af007fde8fefbcbef15feb45ed6519b967390044
baylerblanchard <baylerblanchard@gmail.com>
2024-02-13 08:55:58 -0600

    messing with loading .rb files into irb

--- first.rb
+++ first.rb
@@ -1,1 +1,11 @@
 puts "this is my first ruby program"
+
+name = 'bayler blanchard'
+
+first_name = name.split(' ')[0]
+last_name = name.split(' ')[1]
+
+puts "First name: #{first_name}"
+puts "Last name: #{last_name}"
+puts "Full name: #{first_name} #{last_name}"
+puts "is he hard?"
