c3fd819155ab3cd43ed3107ced4713dda8a59982
baylerblanchard <baylerblanchard@gmail.com>
2024-02-17 14:34:52 -0600

    new commit

--- first.rb
+++ first.rb
@@ -12,20 +12,9 @@ puts "is he hard?"
 
 hard = 0
 
-unless hard < 69
-  puts "yes"
-  puts "#{first_name} is hard"
-  puts "he is hard"
-else
-  puts "no"
-  puts "#{first_name} is not hard"
-  hard +=1
+until hard >= 69
+    hard += 1
+    puts hard 
 end
 
-# array exercises
-a = []
-
-a << 1
-a << first_name
-
-a << last_name
+puts "#{first_name} #{last_name} is finally hard"
