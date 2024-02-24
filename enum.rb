229a254221888cea8a557001fd11a7c303e12dd0
baylerblanchard <baylerblanchard@gmail.com>
2024-02-24 14:44:26 -0600

    used map and each enums

--- enum.rb
+++ enum.rb
@@ -1,5 +1,7 @@
-dynasty_members = ['bayler', 'ben', 'austin', 'justyn', 'brodie', 'jon', 'kadeem']
+dynasty_members = ['bayler', 'ben', 'austin', 'justyn', 'brodie', 'jon', 'kadeem', 'kendra']
 dynasty_admin = []
 
 dynasty_members.each {|member| dynasty_admin.push(member.upcase)}
 puts dynasty_admin
+
+puts dynasty_members.map{|member| member.upcase}
