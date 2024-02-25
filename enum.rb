d6baacb43e007c0a7100296b8ac207e0463345b1
baylerblanchard <baylerblanchard@gmail.com>
2024-02-25 12:26:20 -0600

    more enum practice

--- enum.rb
+++ enum.rb
@@ -5,3 +5,9 @@ dynasty_members.each {|member| dynasty_admin.push(member.upcase)}
 puts dynasty_admin
 
 puts dynasty_members.map{|member| member.upcase}
+
+
+
+puts dynasty_admin
+dynasty_admin.map! {|member| member.downcase}
+puts dynasty_admin
