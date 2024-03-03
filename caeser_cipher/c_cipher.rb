19c6199aaaa18400a4c4791288ae480739f510a5
baylerblanchard <baylerblanchard@gmail.com>
2024-03-03 17:43:27 -0600

    added .ord so i can use the ascii values

--- caeser_cipher/c_cipher.rb
+++ caeser_cipher/c_cipher.rb
@@ -1,1 +1,19 @@
 #methods for the caeser cipher
+
+def caeser_cipher(string1, num)
+    word1 = string1.split('')
+    word1.each {|letter|
+      letter = letter.ord
+      puts (letter)
+    }
+    
+    
+    puts(word1)
+end
+
+
+caeser_cipher('bayler',15)
+
+bayler = 'A'
+
+puts(bayler.ord)
