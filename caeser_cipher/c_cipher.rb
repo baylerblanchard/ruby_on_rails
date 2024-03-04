516515881fc6f407fdbc371c3865469ccfe83e0c
baylerblanchard <baylerblanchard@gmail.com>
2024-03-04 17:08:46 -0600

    added ascii to chr functionallity

--- caeser_cipher/c_cipher.rb
+++ caeser_cipher/c_cipher.rb
@@ -1,18 +1,22 @@
 #methods for the caeser cipher
 
 def caeser_cipher(string1, num)
-    word1 = string1.split('')
-    word1.each {|letter|
-      let_num = letter.ord
-      let_num += num
-      
-      puts (letter.ord)
-      
-      puts (let_num)
-    }
-    
-    
-    puts(word1)
+        word1 = string1.split('')
+        word1.each {|letter|
+            let_num = letter.ord
+            let_num += num
+
+            if let_num > 122
+                let_num -= 26
+            end
+            
+            puts (letter.ord)
+            
+            puts (let_num.chr)
+        }
+        
+        
+        puts(word1)
 end
 
 
