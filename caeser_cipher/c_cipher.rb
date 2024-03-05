278c6ad5db576ecb876c6a0c91164d4ff505215c
baylerblanchard <baylerblanchard@gmail.com>
2024-03-05 14:57:31 -0600

    caeser cipher is complete

--- caeser_cipher/c_cipher.rb
+++ caeser_cipher/c_cipher.rb
@@ -1,23 +1,25 @@
 #methods for the caeser cipher
 
 def caeser_cipher(string1, num)
-        word1 = string1.split('')
-        word1.each {|letter|
+    word1 = string1.split('')
+    word1.map! do |letter|
+        if letter =~ /[a-zA-Z]/
             let_num = letter.ord
             let_num += num
 
-            if let_num > 122
-                let_num -= 26
+            if letter >= 'a' && letter <= 'z'
+                let_num -= 26 if let_num > 'z'.ord
+            elsif letter >= 'A' && letter <= 'Z'
+                let_num -= 26 if let_num > 'Z'.ord
             end
-            
-            puts (letter.ord)
-            
-            puts (let_num.chr)
-        }
-        
-        
-        puts(word1)
+
+            letter = let_num.chr
+        end
+        letter
+    end
+
+    string1 = word1.join
+    puts string1
 end
 
-
-caeser_cipher('bayler',15)
+caeser_cipher('Hello, my name is Bayler',22)
