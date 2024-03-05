bde2b469ab5c7e46d91ad1bbe6f312da3f7d37f2
baylerblanchard <baylerblanchard@gmail.com>
2024-03-05 16:14:04 -0600

    feeling the imposter syndrom kicking in

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,7 +1,14 @@
 #this is my substring project
 
 def substring (words, word_list)
-    match = []
-    
-
+    match = Hash.new(0)
+    words = words.downcase
+    word_list.each do |word|
+        match[word] += words.scan(word).length if words.include?(word)
+    end
+    match
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  
+p substring("Howdy partner, sit down! How's it going?", dictionary)
