daea6e60c4fe55efa226ef3c01bf6a7629a19100
baylerblanchard <baylerblanchard@gmail.com>
2024-03-07 08:55:21 -0600

    added the loops and selection logic

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,1 +1,18 @@
-#this is the stock picker projects
+#this is the stock picker method
+
+def stock_picker(day_prices)
+    profit = 0
+    best_days = []
+
+    day_prices.each_with_index.select do |day, index|
+      puts "day: #{index}, price: #{day}"
+      
+    end
+   
+    
+
+    puts "best days #{best_days}"
+end
+
+
+stock_picker([12,1,45,6,77,3,24])
