dfb16e98074b76df4907fdc15640ee9661c824c3
baylerblanchard <baylerblanchard@gmail.com>
2024-03-14 22:34:30 -0500

    added while loop

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -2,9 +2,15 @@
 
 def bubb_sort(arr) 
     n = arr.length
+    is_swapped = false
+
+    while is_swapped = true
+        puts arr
+        is_swapped = false
+    end
     
 end
 
 
-sort_this = [3,7,2,1,6,8,9,2,5,7,3,1,9]
+sort_this = [3,7,2,1,6,8,0,9,2,5,7,3,1,9]
 bubb_sort(sort_this)
