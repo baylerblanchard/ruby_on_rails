e31405fc7e09e35cdc57ae3f56acd6285a7e33e3
baylerblanchard <baylerblanchard@gmail.com>
2024-03-16 16:49:38 -0500

    more practice

--- example.rb
+++ example.rb
@@ -4,6 +4,28 @@ class GoodDog
   def initialize(name)
     @name = name
   end
+
+  def speak
+    puts "#{@name} lets out a spooky growl"
+  end
+
+  def name
+    @name
+  end
+
+  def name=(n)
+    @name = n
+  end
 end
 
 milo = GoodDog.new("Milo")
+puts milo.speak
+prince = GoodDog.new("Prince")
+puts prince.speak
+
+puts milo.get_name
+puts prince.get_name
+
+prince.set_name("Koa")
+
+puts prince.get_name
