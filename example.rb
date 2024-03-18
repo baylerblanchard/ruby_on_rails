88fadeba7f05e7d2da384e17a161d6f22193c0bb
baylerblanchard <baylerblanchard@gmail.com>
2024-03-18 15:25:32 -0500

    work on the change speed method

--- example.rb
+++ example.rb
@@ -1,14 +1,14 @@
 #this is a new practice file
 
 class MyCar
-  attr_accessor :make, :year, :color
+  attr_accessor :make, :year, :color, :speed
 
   def initialize(m, y, c)
     @make = m
     @year = y
     @color = c
 
-    speed = 0
+    @speed = 0
 
   end
 
@@ -26,6 +26,11 @@ class MyCar
     if ans == "go"
         puts "car is in go mode"
     end
+  end
+
+  def change_speed(n)
+    speed = n
+    puts "speed increases to  #{self.speed}"
   end
 end
 
@@ -67,7 +72,7 @@ puts prince.name
 
 milo.info
 
-
 bayler_car = MyCar.new("toyota", "1999", "red")
 bayler_car.info
 bayler_car.car_state("off")
+bayler_car.change_speed("45")
