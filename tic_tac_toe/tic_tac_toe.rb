4b324ebf6cfa6de2733b3d0a29d0df6ae7bebfcc
baylerblanchard <baylerblanchard@gmail.com>
2024-04-06 12:10:27 -0500

    worked on the display

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -1,5 +1,20 @@
 # this is the tic tac toe project
 
-_|_|_
-_|_|_
- | |
+game_board = Array.new(3) { Array.new(3, ' ') }
+
+game_board[0][0] = '#'
+game_board[0][1] = '#'
+game_board[0][2] = '#'
+game_board[1][0] = '#'
+game_board[1][1] = '#'
+game_board[1][2] = '#'
+game_board[2][0] = '#'
+game_board[2][1] = '#'
+game_board[2][2] = '#'
+
+game_board.each { |row| puts row.join(" | ") }
+puts "\n"
+game_board[1][1] = 'X'
+
+game_board.each { |row| puts row.join(" | ") }
+puts "\n"
