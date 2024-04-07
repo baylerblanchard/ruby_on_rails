d501f406bd6a4b83c48d86dbf30517dee2131a81
baylerblanchard <baylerblanchard@gmail.com>
2024-04-07 16:23:11 -0500

    finished the make move and switch player methods

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -6,7 +6,6 @@ class TicTacToe
   end
 
   def display_board
-    @board[0][1] = 'X'
     @board.each_with_index do |row, i|
       row_display = row.map { |cell| cell.nil? ? ' ' : cell }.join(' | ')
       puts row_display
@@ -14,10 +13,21 @@ class TicTacToe
     end
   end
 
-  def make_move
+  def switch_player
+    if @current_player == 'X'
+      @current_player = 'O'
+    else @current_player = 'X'
+    end
+  end
 
+  def make_move(row, col)
+    @board[row][col] = @current_player
+    display_board
+    switch_player
   end
 end
 
 game = TicTacToe.new
 game.display_board
+game.make_move(1, 1)
+game.make_move(0, 0)
