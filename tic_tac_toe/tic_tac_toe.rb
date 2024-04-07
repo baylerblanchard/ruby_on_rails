3ad6bc77db6a6dbf050e06f5531de0a5f70dd29a
baylerblanchard <baylerblanchard@gmail.com>
2024-04-07 15:17:30 -0500

    restarting this project from scratch

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -1,25 +1,4 @@
-# this is the tic tac toe project
-
-game_board = Array.new(3) { Array.new(3, ' ') }
-
-game_board[0][0] = '#'
-game_board[0][1] = '#'
-game_board[0][2] = '#'
-game_board[1][0] = '#'
-game_board[1][1] = '#'
-game_board[1][2] = '#'
-game_board[2][0] = '#'
-game_board[2][1] = '#'
-game_board[2][2] = '#'
-
-game_board.each { |row| puts row.join(" | ") }
-puts "\n"
-game_board[1][1] = 'X'
-
-game_board.each { |row| puts row.join(" | ") }
-puts "\n"
-
-# tic tac toe class for the game
+# class for tictactoe game
 class TicTacToe
   def initialize
     @board = Array.new(3) { Array.new(3, ' ') }
@@ -27,43 +6,14 @@ class TicTacToe
   end
 
   def display_board
-    @board.each { |row| puts row.join(" | ") }
-  end
-
-  def make_move(row, col)
-    if @board[row][col] = ' '
-      @board[row, col] = @current_player
-      switch_player
-    else
-      puts 'invalid move please try again'
-    end
-  end
-
-  def switch_player
-    @current_player = current_player == 'X' ? 'O' : 'X'
-  end
-
-  def check_row_col
+    @board[0][1] = 'X'
     @board.each_with_index do |row, i|
-      col = @board.map { |r| r[i] }
-      return true if row.uniq.length == 1 && row[0] != ' '
-      return true if col.uniq.length == 1 && col[0] != ' '
+      row_display = row.map { |cell| cell.nil? ? ' ' : cell }.join(' | ')
+      puts row_display
+      puts '-' * (row_display.length + 2) unless i == @board.length - 1
     end
-  end
-
-  def check_diag
-    diagonals = [
-      [@board[0][0], @board[1][1], @board[2][2]],
-      [@board[0][2], @board[1][1], @board[2][0]]
-    ]
-    diagonals.each do |diag|
-      return true if diag.uniq.length == 1 && diag[0] != ' '
-    end
-    false
-  end
-
-  def check_winner
-    check_diag
-    check_row_col
   end
 end
+
+game = TicTacToe.new
+game.display_board
