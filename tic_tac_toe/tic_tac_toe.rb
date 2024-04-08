30f03c29d49722fe6a018d5615c0be5385acf92b
baylerblanchard <baylerblanchard@gmail.com>
2024-04-08 15:19:07 -0500

    finished the diagonal check

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -11,6 +11,7 @@ class TicTacToe
       puts row_display
       puts '-' * (row_display.length + 2) unless i == @board.length - 1
     end
+    puts "\n\n"
   end
 
   def switch_player
@@ -21,10 +22,22 @@ class TicTacToe
     end
   end
 
-  def check_winner
+  def check_diag
     diagonals = [
-
+      @board[0][0], @board[1][1], @board[2][2],
+      @board[0][2], @board[1][1], @board[2][0]
     ]
+
+    diagonals.each do |diag|
+      return true if diag.uniq.length == 1 && diag[0] != ''
+    end
+    false
+  end
+
+  def check_win
+    if check_diag = true
+      puts "the diagonal wins"
+    end
   end
 
   def make_move(row, col)
@@ -37,4 +50,8 @@ end
 game = TicTacToe.new
 game.display_board
 game.make_move(1, 1)
+game.make_move(0, 1)
 game.make_move(0, 0)
+game.make_move(0, 2)
+game.make_move(2, 2)
+game.check_win
