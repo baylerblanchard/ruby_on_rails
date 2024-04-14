1b351d230589623ff8dda40bd70a53b801894a95
baylerblanchard <baylerblanchard@gmail.com>
2024-04-14 17:11:17 -0500

    working on the masterminds project

--- mastermind/mm.rb
+++ mastermind/mm.rb
@@ -1,1 +1,18 @@
 # this is my mastermind game coded in ruby
+
+class Mastermind
+  attr_accessor :key
+  def initialize
+    @key
+  end
+
+  def create_key
+    @key = []
+    color_list = ['red', 'blue', 'green', 'yellow', 'black', 'brown', 'white', 'grey', 'orange', 'pink', 'purple']
+    @key = color_list.sample(4)
+  end
+end
+
+game = Mastermind.new
+game.create_key
+puts game.key
