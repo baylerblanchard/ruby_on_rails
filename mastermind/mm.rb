5b974090c84361cbfcc658a2ea69b9dc474539e3
baylerblanchard <baylerblanchard@gmail.com>
2024-04-15 11:17:41 -0500

    workin on the mastermind project

--- mastermind/mm.rb
+++ mastermind/mm.rb
@@ -2,6 +2,7 @@
 
 class Mastermind
   attr_accessor :key
+
   def initialize
     @key
   end
@@ -11,8 +12,20 @@ class Mastermind
     color_list = ['red', 'blue', 'green', 'yellow', 'black', 'brown', 'white', 'grey', 'orange', 'pink', 'purple']
     @key = color_list.sample(4)
   end
-end
 
+  def play_game
+    game = Mastermind.new
+    game.create_key
+    i = 0
+    guess_attempt = 0
+
+    for i in 12
+      puts "guess number: #{i}"
+      i += i
+    end
+
+  end
+end
 game = Mastermind.new
-game.create_key
+game.play_game
 puts game.key
