5b16c1d1cc7ade4e3d6f117f881cebb84441907a
baylerblanchard <baylerblanchard@gmail.com>
2024-04-16 16:21:12 -0500

    have the create key method working

--- mastermind/mm.rb
+++ mastermind/mm.rb
@@ -11,21 +11,10 @@ class Mastermind
     @key = []
     color_list = ['red', 'blue', 'green', 'yellow', 'black', 'brown', 'white', 'grey', 'orange', 'pink', 'purple']
     @key = color_list.sample(4)
+    puts 'this worked'
   end
 
-  def play_game
-    game = Mastermind.new
-    game.create_key
-    i = 0
-    guess_attempt = 0
-
-    for i in 12
-      puts "guess number: #{i}"
-      i += i
-    end
-
-  end
 end
 game = Mastermind.new
-game.play_game
+game.create_key
 puts game.key
