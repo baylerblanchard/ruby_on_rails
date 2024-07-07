dd9d30874f7b8c9e88d25438e8d3f18c6fe90a8d
baylerblanchard <baylerblanchard@gmail.com>
2024-07-07 11:12:18 -0500

    finish the if statement. we are back and going to finish these projects

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -13,6 +13,27 @@ class HangmanGame
     @word_blank = '_ ' * @word.length
   end
 
+  def save_game
+    File.open('hangman_save.txt', 'w') do |file|
+      file.puts(self.instance_variables.map { |var| "#{var}=#{self.instance_variable_get(var).inspect}" unless var == :@words })
+    end
+  end
+
+  # Open the saved game file
+  def self.open_save
+    game = self.new
+    File.open('hangman_save.txt', 'r') do |file|
+      # Read the saved game state
+      saved_game_state = file.read
+      # Process the saved game state as saved_game_state = file.read.split("\n")
+      saved_game_state.each do |var|
+        var_name, var_value = var.split('=')
+        game.instance_variable_set(var_name, eval(var_value))
+      end
+    end
+    game
+  end
+
   def play_game
     while @attempts.positive?
       puts "Attempts left: #{@attempts}"
@@ -56,24 +77,12 @@ class HangmanGame
   end
 end
 
+puts "is this a new game?Y/N"
+ans = gets.chomp.downcase
+
+if ans =
+
 game = HangmanGame.new
 puts game.instance_variable_get(:@word)
-
-# Save the game state to a file
-def save_game
-  File.open('hangman_save.txt', 'w') do |file|
-    file.puts(game.instance_variables.map { |var| "#{var}=#{game.instance_variable_get(var).inspect}" unless var == :@words })
-  end
-end
-
-# Open the saved game file
-def open_save
-  File.open('hangman_save.txt', 'r') do |file|
-    # Read the saved game state
-    saved_game_state = file.read
-    # Process the saved game state as needed
-    # ...
-  end
-end
 
 game.play_game
