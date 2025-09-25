a4d21ecc2031fa24181acc4a1c729b33fe99f060
baylerblanchard <baylerblanchard@gmail.com>
2025-09-25 12:47:36 -0500

    finished merge sort but needed some syntax help to complete it. I understood the requirements but had trouble putting it to code.

--- merge_sort/merge_sort.rb
+++ merge_sort/merge_sort.rb
@@ -1,15 +1,33 @@
-arr = [98,12,76,23]
+arr = [98, 12, 76, 23, 8, 58, 37, 42, 69, 420, 32, 72, 12, 63, 90, 902, 25, 77]
 
-def merge_sort(arr)
-  if arr.length <= 1
-    return arr
+def merge_sort(a)
+  if a.length <= 1
+    return a
   end
-  mid = arr.length / 2
-  left_arr = arr[0..mid-1]
-  right_arr = arr[mid..arr.length]
+  mid = a.length / 2
+  left_arr = a[0...mid]
+  right_arr = a[mid..-1]
 
-  p left_arr
-  p right_arr
+  sorted_left = merge_sort(left_arr)
+  sorted_right = merge_sort(right_arr)
+
+  sort(sorted_left, sorted_right)
 end
 
-merge_sort(arr)
+
+def sort(a,b)
+  sorted_array = []
+  while !a.empty? && !b.empty?
+    if a.first <= b.first
+      sorted_array << a.shift
+    else
+      sorted_array << b.shift
+    end
+  end
+  return sorted_array.concat(a).concat(b)
+end
+
+sorted_arr = merge_sort(arr)
+
+puts "unsorted: #{arr.inspect}"
+puts "sorted: #{sorted_arr.inspect}"
