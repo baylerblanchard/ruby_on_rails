7bfa2f2b417d478a8eff701f6aaf1e7d0ef9f88c
baylerblanchard <baylerblanchard@gmail.com>
2025-09-24 23:51:26 -0500

    making progress on the merge sort

--- merge_sort/merge_sort.rb
+++ merge_sort/merge_sort.rb
@@ -1,7 +1,18 @@
-def merge_sort[arr]
-  len = arr.length
+arr = [98,12,76,23,73,28,63,2,1]
+
+def merge_sort(arr)
   if arr.length <= 1
     return arr
   end
-  
+  mid = arr.length / 2
+  left_arr = arr[0..mid-1]
+  right_arr = arr[mid..arr.length]
+
+  p left_arr
+  p right_arr
+  merge_sort(left_arr)
+  merge_sort(right_arr)
+  return
 end
+
+merge_sort(arr)
