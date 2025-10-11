60068496d54e88eb72a5400378763091aced5dd3
baylerblanchard <baylerblanchard@gmail.com>
2025-10-10 22:16:09 -0500

    finished the node class and need to work on the linkedlist class functions tomorrow

--- linked_list.rb
+++ linked_list.rb
@@ -1,7 +1,20 @@
-def node
-  
+# frozen_string_literal: true
+
+#-- Node Class for my linked list practice --#
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
 end
 
-def linked_list
-  
+#-- class for linked list, need to add the other functions tomorrow at work --#
+class LinkedList
+  attr_accessor :head
+
+  def initialize
+    @head = nil
+  end
 end
