64fb0a656ea89a3850690eacc90346c59ffd508a
baylerblanchard <baylerblanchard@gmail.com>
2025-11-03 13:58:40 -0600

    starting the binary search tree

---
+++ bst.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Node class for the nodes of the tree
+class Node
+  attr_accessor :left, :righ, :data
+
+  def initialize(data)
+    @data = data
+    @left = nil
+    @right = nil
+  end
+
+end
