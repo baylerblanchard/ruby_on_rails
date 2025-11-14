06841912cefed6bfc65eeb51983e39e68dff23f5
baylerblanchard <baylerblanchard@gmail.com>
2025-11-13 22:18:28 -0600

    added the pretty print function and fixed the issue with the right nodes

--- bst.rb
+++ bst.rb
@@ -2,7 +2,7 @@
 
 # Node class for the nodes of the tree
 class Node
-  attr_accessor :left, :righ, :data
+  attr_accessor :left, :right, :data
 
   def initialize(data)
     @data = data
@@ -21,15 +21,27 @@ class Tree
   end
 
   def build_tree(array, start_index, end_index)
-    mid_index = array.length / 2
+    if start_index > end_index
+      return nil
+    end
+
+    mid_index = (start_index + end_index) / 2
     new_node = Node.new(array[mid_index])
 
     new_node.left = build_tree(array, start_index, mid_index - 1)
     new_node.right = build_tree(array, mid_index + 1, end_index)
+
+    return new_node
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
 end
 
 mytree = Tree.new([43, 23, 63, 1, 52, 85, 21, 87, 443, 643, 34, 555])
 
-p mytree.root
-p mytree
+mytree.pretty_print
