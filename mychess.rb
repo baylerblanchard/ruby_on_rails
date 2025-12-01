1fa753f9ebe8626f9e16a9d1263a3145713cb081
baylerblanchard <baylerblanchard@gmail.com>
2025-12-01 11:30:19 -0600

    worked on the print board fucntion and next will add the pieces

--- mychess.rb
+++ mychess.rb
@@ -1,23 +1,36 @@
 class Chess
-  attr_accessor :row, :column, :player1, :player2
+  attr_accessor :player1, :player2
+  attr_reader :board
 
   def initialize
-    @row = 8
-    @column = 8
-    @player1 = 'new'
-    @player2 = 'player'
+    # An 8x8 board, initialized with empty spaces ' '
+    @board = Array.new(8) { Array.new(8, ' ') }
+    @player1 = 'Player 1'
+    @player2 = 'Player 2'
 
   end
 
   def player_choice
-    first_turn = [player1, player2].sample
-    first_turn
+    [@player1, @player2].sample
+  end
+
+  def print_board
+    puts
+    puts "   a  b  c  d  e  f  g  h"
+    puts "  +--+--+--+--+--+--+--+--+"
+    @board.each_with_index do |row, index|
+      # Print row numbers from 8 down to 1
+      print "#{8 - index} |"
+      # Join the squares in the row with a separator
+      print row.join(' |')
+      puts " |"
+      puts "  +--+--+--+--+--+--+--+--+"
+    end
+    puts
   end
 end
 
-
 new_chess = Chess.new
 
-puts new_chess.row
-puts new_chess.column
 puts new_chess.player_choice
+new_chess.print_board
