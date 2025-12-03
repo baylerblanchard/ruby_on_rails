0a4ed3d1d111a5bea964e76ff07c2db5921e7bb1
baylerblanchard <77814664+baylerblanchard@users.noreply.github.com>
2025-12-02 22:39:41 -0600

    Refactor chess game with class structure and display
    Refactor chess game structure by introducing Piece, Rook, Board, and Game classes. Update board display and piece initialization.

--- mychess.rb
+++ mychess.rb
@@ -1,36 +1,84 @@
-class Chess
-  attr_accessor :player1, :player2
-  attr_reader :board
+# A parent class for all pieces
+class Piece
+  attr_reader :color
 
-  def initialize
-    # An 8x8 board, initialized with empty spaces ' '
-    @board = Array.new(8) { Array.new(8, ' ') }
-    @player1 = 'Player 1'
-    @player2 = 'Player 2'
-
+  def initialize(color)
+    @color = color
   end
 
-  def player_choice
-    [@player1, @player2].sample
-  end
-
-  def print_board
-    puts
-    puts "   a  b  c  d  e  f  g  h"
-    puts "  +--+--+--+--+--+--+--+--+"
-    @board.each_with_index do |row, index|
-      # Print row numbers from 8 down to 1
-      print "#{8 - index} |"
-      # Join the squares in the row with a separator
-      print row.join(' |')
-      puts " |"
-      puts "  +--+--+--+--+--+--+--+--+"
-    end
-    puts
+  # We'll use Unicode characters for a nicer display
+  def to_s
+    ' ' # Placeholder
   end
 end
 
-new_chess = Chess.new
+# Example for a Rook
+class Rook < Piece
+  def to_s
+    @color == :white ? '♖' : '♜'
+  end
+end
 
-puts new_chess.player_choice
-new_chess.print_board
+# You would create similar classes for King, Queen, Bishop, Knight, and Pawn.
+class Board
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(8) { Array.new(8) }
+    setup_pieces
+  end
+
+  def display
+    puts "  a b c d e f g h"
+    @grid.each_with_index do |row, i|
+      print "#{8 - i} "
+      puts row.map { |piece| piece.nil? ? '.' : piece.to_s }.join(' ')
+    end
+  end
+
+  private
+
+  def setup_pieces
+    # This is a simplified setup. You would place all pieces.
+    # Place Rooks
+    @grid[0][0] = Rook.new(:black)
+    @grid[0][7] = Rook.new(:black)
+    @grid[7][0] = Rook.new(:white)
+    @grid[7][7] = Rook.new(:white)
+
+    # ... and so on for Knights, Bishops, Queens, Kings, and Pawns.
+  end
+end
+class Game
+  def initialize
+    @board = Board.new
+    @current_player = :white
+  end
+
+  def play
+    loop do
+      @board.display
+      puts "It's #{@current_player}'s turn."
+      puts "Enter your move (e.g., 'e2e4'):"
+      
+      move = gets.chomp
+      # TODO: Parse the move string (e.g., 'e2e4') into coordinates.
+      # TODO: Validate the move based on chess rules.
+      # TODO: Update the board state.
+      
+      puts "Move received: #{move}" # Placeholder
+      
+      switch_player
+    end
+  end
+
+  private
+
+  def switch_player
+    @current_player = (@current_player == :white) ? :black : :white
+  end
+end
+
+# To start the game:
+# game = Game.new
+# game.play
